0616d287ff5a9e99574e0bce9884a9c6373c61f6
Abdallah <abdallahchaar90@gmail.comh>
2021-01-15 11:29:21 +0100

    hur vi skriver flera varieabl och hur vi kan skriva den kort med change

---
+++ main.js
@@ -0,0 +1,33 @@
+/* glopal console, alert , prompt*/
+/*
+var myNumber = 120,
+
+    myString = myNumber.toString();
+
+console.log(myNumber);
+
+console.log(myString);
+
+
+var myNewNumber = myString.replace(2, 3);
+
+console.log(myNewNumber);
+
+
+var myLastNumber = myNewNumber.split("");
+
+console.log(myLastNumber);
+
+*/
+
+var myNumber = 120,
+
+    myString = myNumber.toString().replace(2, 3).split("");
+
+    console.log(myNumber);
+
+    console.log(myString);
+
+
+
+
